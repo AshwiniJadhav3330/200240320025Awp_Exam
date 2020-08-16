fb8df308eb169f1bffe8aae1f67138d8d6871f83
AshwiniJadhav3330 <65149590+AshwiniJadhav3330@users.noreply.github.com>
2020-08-16 12:19:03 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,26 @@
+function validation()
+{
+
+	const user1 = document.getElementById('#user');
+	const pass1 = document.getElementById('#pass');
+	const mail1 = document.getElementById('#mail');
+	
+	
+	 if(user1 == "")
+	{
+		alert(' InValid Information');
+	}
+	
+	 
+	 const user2 = document.createElement('#user').value;
+	const pass2 = document.createElement('#pass').value;
+	const mail2 = document.createElement('#mail').value;
+	
+	user2.textContent(d);
+	pass2.textContent(d);
+	mail2.textContent(d);
+	
+	d.document.querySelector('#d');
+	
+	
+}
